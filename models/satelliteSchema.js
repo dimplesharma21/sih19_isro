61aa5ad7145684ce2eb917c99be59c030b12228d
dimple sharma <dimpleryp@gmail.com>
2019-03-03 09:42:24 +0530

    default added

--- models/satelliteSchema.js
+++ models/satelliteSchema.js
@@ -1,11 +1,12 @@
 var mongoose = require('mongoose');
 
 var satelliteSchema = mongoose.Schema({
-  name: String,
-  code: String,
-  type: String,
-  longitude: Number,
-  latitude: {type: Number, default: 0}
+  name: {type: String, default: 'GSAT-14'},
+  code: {type: String, default: 's009'},
+  type: {type: String, default: 'GSAT'},
+  longitude: {type: Number, default: 85},
+  latitude: {type: Number, default: 0},
+  power: {type: Number, default: 0}
 });
 
 module.exports = mongoose.model('Satellite', satelliteSchema);
--- models/terminalSchema.js
+++ models/terminalSchema.js
@@ -1,14 +1,14 @@
 var mongoose = require('mongoose');
 
 var terminalSchema = mongoose.Schema({
-  name: String,
-  code: String,
-  state: String,
-  district: String,
-  pincode: Number,
-  datarate: Number,
-  transmission_power: Number,
-  type: String
+  name: {type: String, default: 'vsat-007'},
+  code: {type: String, default: 'v007'},
+  state: {type: String, default:'Tamil Nadu'},
+  district: {type: String, default: 'Chennai'},
+  pincode: {type: Number, default: 789002},
+  datarate: {type: Number, default: 5},
+  transmission_power: {type: Number, default: 0},
+  type: {type: String, default: GSAT}
 });
 
 module.exports = mongoose.model('Terminal', terminalSchema);
